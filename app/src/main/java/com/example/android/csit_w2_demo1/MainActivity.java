a441c50e30c7f59831c5ba7bd1e48ffdf427122b
priyankabp <pbphapale@gmail.com>
2017-01-31 12:36:28 -0500

    Add methods to understand Android  lifecycle

--- app/src/main/java/com/example/android/csit_w2_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit_w2_demo1/MainActivity.java
@@ -37,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    
     class MyLsnr implements View.OnClickListener {
 
         @Override

4a4e9af6509cdf3835ed6b41373631e2fb738e44
priyankabp <pbphapale@gmail.com>
2017-01-31 12:07:43 -0500

    Add MyLsnr to change text on button click

--- app/src/main/java/com/example/android/csit_w2_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit_w2_demo1/MainActivity.java
@@ -16,13 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.tv_1_act1);
-        button = (Button)findViewById(R.id.btn1_act1);
-        button.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView) findViewById(R.id.tv_1_act1);
+        button = (Button) findViewById(R.id.btn1_act1);
+        button.setOnClickListener(new MyLsnr());
+       /* button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textView.setText("Some other text");
             }
-        });
+        });*/
+    }
+
+    class MyLsnr implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            textView.setText("Some other text");
+        }
     }
 }
+
